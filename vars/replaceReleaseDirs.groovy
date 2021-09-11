d6c7e4165a71ea03daab31bcff16d3d747903824
HaruAtari <reg@haru-atari.com>
2021-09-11 11:04:23 +0300

    Release the `replaceReleaseDirs` script

--- vars/replaceReleaseDirs.groovy
+++ vars/replaceReleaseDirs.groovy
@@ -1,18 +1,18 @@
 #!/usr/bin/env groovy
 
-def call(String sourceDir, String destinationDir) {
+def call(String baseDir) {
   // Init directory structure for the first start
-  sh "mkdir -p ${destinationDir}/release-prev"
-  sh "mkdir -p ${destinationDir}/release"
+  sh "mkdir -p ${baseDir}/release-prev"
+  sh "mkdir -p ${baseDir}/release"
 
   // Copy source files
-  sh "rm -r -f ${destinationDir}/release-next"
-  sh "cp -r '${sourceDir}' ${destinationDir}/release-next"
+  sh "rm -r -f ${baseDir}/release-next"
+  sh "cp -r '${env.WORKSPACE}' ${baseDir}/release-next"
 
   // Replace the current release by the built
-  sh "rm -r -f ${destinationDir}/release-prev"
-  sh "mv ${destinationDir}/release ${destinationDir}/release-prev"
-  sh "mv ${destinationDir}/release-next ${destinationDir}/release"
-  sh "ln -s ${destinationDir}/release ${destinationDir}/site"
-  sh "rm -r -f ${destinationDir}/release-prev"
+  sh "rm -r -f ${baseDir}/release-prev"
+  sh "mv ${baseDir}/release ${baseDir}/release-prev"
+  sh "mv ${baseDir}/release-next ${baseDir}/release"
+  sh "ln -s ${baseDir}/release ${baseDir}/site"
+  sh "rm -r -f ${baseDir}/release-prev"
 }
