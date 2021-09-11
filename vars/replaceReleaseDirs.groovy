fa6aad6740ce98337804f3b9513258156f8f8bcd
HaruAtari <reg@haru-atari.com>
2021-09-11 10:32:21 +0300

    Small improve

--- vars/replaceReleaseDirs.groovy
+++ vars/replaceReleaseDirs.groovy
@@ -1,5 +1,19 @@
 #!/usr/bin/env groovy
 
-def call(String name = 'human') {
+def call(String baseDir) {
+  // Init directory structure for the first start
+  sh "mkdir -p ${baseDir}/release-prev"
+  sh "mkdir -p ${baseDir}/release"
+
+  // Copy source files
+  sh "rm -r -f ${baseDir}/release-next"
+  sh "cp -r '${env.WORKSPACE}' ${baseDir}/release-next"
+
+  // Replace the current release by the built
+  sh "rm -r -f ${baseDir}/release-prev"
+  sh "mv ${baseDir}/release ${baseDir}/release-prev"
+  sh "mv ${baseDir}/release-next ${baseDir}/release"
+  sh "ln -s ${baseDir}/release ${baseDir}/site"
+  sh "rm -r -f ${baseDir}/release-prev"
   echo "Hello, ${name}."
 }
