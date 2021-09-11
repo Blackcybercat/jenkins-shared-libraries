5b78bd71368545953e9ba011dc93ac8cf01df020
HaruAtari <reg@haru-atari.com>
2021-09-11 12:21:40 +0300

    Add teh `sendStatusChangedDiscordNotification` script

---
+++ vars/sendStatusChangedDiscordNotification.groovy
@@ -0,0 +1,11 @@
+#!/usr/bin/env groovy
+
+def call(Boolean isSuccess) {
+  colour=15548997
+  text= 'Build was failed'
+  if(isSuccess) {
+    colour=5763719
+    text='Build was restored'
+  }
+  sh """curl -X POST -H "Content-Type: application/json" -d '{"embeds":[{"title": "${env.JOB_NAME}", "description": "${text}", "color": ${colour}]}' -H "Content-Type:application/json" ${env.discordMonitoringWebhookUrl}"""
+}
