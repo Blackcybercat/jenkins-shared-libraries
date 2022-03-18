604d558fe883ca956d66e95ee6ba5f622f039604
HaruAtari <reg@haru-atari.com>
2022-03-18 13:23:51 +0300

    Improve a `createDatabase` script

--- vars/createDatabase.groovy
+++ vars/createDatabase.groovy
@@ -6,7 +6,7 @@ def call(String dbName, Boolean dropExisted = false) {
   if (dropExisted) {
     sh "psql -U $postgresUser -d 'postgres' -c \"DROP DATABASE IF EXISTS $dbName \""
   }
-if(sh (script: "psql -U $postgresUser -d 'postgres' -tc \"SELECT 1 FROM pg_database WHERE datname = '$dbName'\" | grep -q 1", returnStatus: true)){
-  sh "psql -U $postgresUser -d 'postgres' -c \"CREATE DATABASE $dbName \""
+  if(sh (script: "psql -U $postgresUser -d 'postgres' -tc \"SELECT 1 FROM pg_database WHERE datname = '$dbName'\" | grep -q 1", returnStatus: true)){
+    sh "psql -U $postgresUser -d 'postgres' -c \"CREATE DATABASE $dbName \""
   }
 }
